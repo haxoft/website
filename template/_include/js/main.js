b2d109ea9f97854ad160fa8b9f81d8439ad145cc
Valentin <versus740@gmail.com>
2016-10-19 00:53:28 +0200

    Logo is now a more inline as before in the slider

--- template/_include/js/main.js
+++ template/_include/js/main.js
@@ -77,10 +77,10 @@ BRUSHED.slider = function(){
 		thumb_links				:	0,			// Individual thumb links for each slide
 		thumbnail_navigation    :   0,			// Thumbnail navigation
 		slides 					:  	[			// Slideshow Images
-											{image : '_include/img/slider-images/image01.jpg', title : '<div class="slide-content">HA<span class="content-red">X</span>OFT<img src="_include/img/logo/HaXoft.png" align="top" width="45" height="45"></div>', thumb : '', url : ''},
-											{image : '_include/img/slider-images/image02.jpg', title : '<div class="slide-content">HA<img src="_include/img/logo/HaXoft.png" align="middle" width="50" height="50">OFT</div>', thumb : '', url : ''},
-											{image : '_include/img/slider-images/image03.jpg', title : '<div class="slide-content">HA<img src="_include/img/logo/HaXoft.png" align="middle" width="50" height="50">OFT</div>', thumb : '', url : ''},
-											{image : '_include/img/slider-images/image04.jpg', title : '<div class="slide-content">HA<img src="_include/img/logo/HaXoft.png" align="middle" width="50" height="50">OFT</div>', thumb : '', url : ''}
+											{image : '_include/img/slider-images/image01.jpg', title : '<div class="slide-content"><p>HA<span class="content-red">X</span>OFT<img src="_include/img/logo/HaXoft.png" align="top" width="45" height="45"/></p></div>', thumb : '', url : ''},
+											{image : '_include/img/slider-images/image02.jpg', title : '<div class="slide-content"><p>HA<span class="content-red">X</span>OFT<img src="_include/img/logo/HaXoft.png" align="top" width="45" height="45"/></p></div>', thumb : '', url : ''},
+											{image : '_include/img/slider-images/image03.jpg', title : '<div class="slide-content"><p>HA<span class="content-red">X</span>OFT<img src="_include/img/logo/HaXoft.png" align="top" width="45" height="45"/></p></div>', thumb : '', url : ''},
+											{image : '_include/img/slider-images/image04.jpg', title : '<div class="slide-content"><p>HA<span class="content-red">X</span>OFT<img src="_include/img/logo/HaXoft.png" align="top" width="45" height="45"/></p></div>', thumb : '', url : ''}
 									],
 									
 		// Theme Options			   
